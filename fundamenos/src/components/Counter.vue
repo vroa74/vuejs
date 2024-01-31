896042b4ca8eb350f092e64cc65c26a9a6e8e93f
vroa74 <33233830+vroa74@users.noreply.github.com>
2024-01-31 08:31:59 -0600

    48

---
+++ fundamenos/src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+   <h1> i'm Darth Vader</h1>
+  <p>lord sith master of dark side</p>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style scoped>
+
+</style>
